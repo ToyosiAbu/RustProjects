d84ee5baa65c9bf9f413476db6e377ff2a43297d
Omotoyosi Abu <omotoyosiabu@Omotoyosis-MacBook-Air.local>
2025-03-16 17:41:46 -0700

    Restructuring Files To Allow For Multiple Projects

---
+++ hello_cargo/target/debug/deps/hello_cargo-3aaeed6b087dd5ce.d
@@ -0,0 +1,5 @@
+/Users/omotoyosiabu/Documents/Rust/projects/hello_cargo/target/debug/deps/hello_cargo-3aaeed6b087dd5ce: src/main.rs
+
+/Users/omotoyosiabu/Documents/Rust/projects/hello_cargo/target/debug/deps/hello_cargo-3aaeed6b087dd5ce.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/omotoyosiabu/Documents/Rust/projects/hello_cargo/target/debug/hello_cargo: /Users/omotoyosiabu/Documents/Rust/projects/hello_cargo/src/main.rs
